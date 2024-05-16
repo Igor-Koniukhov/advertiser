6054bb6d17a8925f99d06677dc87aae94ae65111
Ihor Koniukhov <ihor.koniukhov@nixsolutions.com>
2024-05-16 19:07:28 +0300

    add fixed dashboard

--- src/features/adds/dashboard/AddsDashboard.tsx
+++ src/features/adds/dashboard/AddsDashboard.tsx
@@ -3,10 +3,10 @@ import AddsList from "./AddsList.tsx"
 import { useAppSelector } from "@/app/store/store.ts"
 import { useEffect, useState } from "react"
 import { actions } from "@/features/adds/addsSlice.ts"
-import { LoadingComponent } from "@/app/layout/LoadingComponent.tsx"
 import { useFirestore } from "@/app/hooks/firestore/useFirestore.ts"
 import { AddsFilters } from "@/features/adds/dashboard/AddsFilters.tsx"
 import { QueryOptions } from "@/app/hooks/firestore/hooks.ts"
+import AddListItemPlaceholder from "@/features/adds/dashboard/AddListItemPlaceholder.tsx"
 
 export default function AddsDashboard() {
   const { data: adds, status } = useAppSelector((state) => state.adds)
@@ -20,11 +20,18 @@ export default function AddsDashboard() {
       queries: query,
     })
   }, [loadCollection, query])
-  if (status === "loading") return <LoadingComponent />
+
   return (
     <Grid>
       <Grid.Column width={10}>
-        <AddsList events={adds} />
+        {status === "loading" ? (
+          <>
+            <AddListItemPlaceholder />
+            <AddListItemPlaceholder />
+          </>
+        ) : (
+          <AddsList events={adds} />
+        )}
       </Grid.Column>
       <Grid.Column width={6}>
         <AddsFilters setQuery={setQuery} />
